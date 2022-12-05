ca93d1623174c929fc6490d30a33ce8faee1cfe5
josborn5 <josborn5@gmail.com>
2022-12-04 21:32:10 -0600

    Moving obby block parsing to common function.

--- src/obby/obby-2/update-gamestate.cpp
+++ src/obby/obby-2/update-gamestate.cpp
@@ -11,12 +11,6 @@ float maxPlayerX;
 float minPlayerY;
 float maxPlayerY;
 
-static void ClearBlock(Block* block)
-{
-	block->exists = false;
-	block->color = 0;
-}
-
 int PopulateBlocksForLevel(
 	int level,
 	GameState &gameState,
@@ -31,69 +25,7 @@ int PopulateBlocksForLevel(
 	// level is a 1-based index. levels array is 0-based.
 	char* blockLayout = levels[level - 1]; // levels is a global var set by levels.cpp .. TODO: make not global
 
-	tl::Vec2<int> dimensions = tl::GetContentDimensions(blockLayout);
-
-	// Check the block array size is big enough for the content
-	int contentCount = dimensions.x * dimensions.y;
-	if (contentCount > gameState.blockCount)
-	{
-		return 1;
-	}
-
-	float blockHeight = (float)pixelRect.y / dimensions.y;
-	float blockWidth = (float)pixelRect.x / dimensions.x;
-	tl::Vec2<float> blockHalfSize = {
-		0.5f * blockWidth,
-		0.5f * blockHeight
-	};
-
-	bool endOfContent = false;
-	float originalX = blockHalfSize.x;
-	tl::Vec2<float> blockPosition = {
-		originalX,
-		(float)pixelRect.y - blockHalfSize.y
-	};
-	for (int i = 0; i < gameState.blockCount; i += 1)
-	{
-		endOfContent = (*blockLayout == NULL);
-
-		if (*blockLayout == '\n')
-		{
-			blockPosition.x = originalX;
-			blockPosition.y -= blockHeight;
-		}
-		else
-		{
-			Block* block = &(gameState.blocks[i]);
-			if (endOfContent)
-			{
-				ClearBlock(block);
-			}
-			else if (*blockLayout != ' ')
-			{
-				block->exists = true;
-				bool isCheckpoint = (*blockLayout == 'c');
-				block->color = (isCheckpoint) ? 0xAA5555 : 0xAAAAAA;
-				block->isCheckpoint = isCheckpoint;
-			}
-			else
-			{
-				ClearBlock(block);
-			}
-
-			block->halfSize = blockHalfSize;
-			block->position = blockPosition;
-
-			blockPosition.x += blockWidth;
-		}
-
-		if (!endOfContent)
-		{
-			blockLayout++;
-		}
-	}
-
-	return 0;
+	return PopulateBlocksForLevelString(blockLayout, gameState, pixelRect);
 }
 
 static int StartLevel(int newLevel, const tl::Vec2<int> &pixelRect)
--- src/obby/platform_common.cpp
+++ src/obby/platform_common.cpp
@@ -26,3 +26,78 @@ static float ClampFloat(float min, float val, float max)
 	return val;
 }
 
+static void ClearBlock(Block* block)
+{
+	block->exists = false;
+	block->color = 0;
+}
+
+int PopulateBlocksForLevelString(
+	char* blockLayout,
+	GameState &gameState,
+	const tl::Vec2<int> &pixelRect
+) {
+	tl::Vec2<int> dimensions = tl::GetContentDimensions(blockLayout);
+
+	// Check the block array size is big enough for the content
+	int contentCount = dimensions.x * dimensions.y;
+	if (contentCount > gameState.blockCount)
+	{
+		return 1;
+	}
+
+	float blockHeight = (float)pixelRect.y / dimensions.y;
+	float blockWidth = (float)pixelRect.x / dimensions.x;
+	tl::Vec2<float> blockHalfSize = {
+		0.5f * blockWidth,
+		0.5f * blockHeight
+	};
+
+	bool endOfContent = false;
+	float originalX = blockHalfSize.x;
+	tl::Vec2<float> blockPosition = {
+		originalX,
+		(float)pixelRect.y - blockHalfSize.y
+	};
+	for (int i = 0; i < gameState.blockCount; i += 1)
+	{
+		endOfContent = (*blockLayout == NULL);
+
+		if (*blockLayout == '\n')
+		{
+			blockPosition.x = originalX;
+			blockPosition.y -= blockHeight;
+		}
+		else
+		{
+			Block* block = &(gameState.blocks[i]);
+			if (endOfContent)
+			{
+				ClearBlock(block);
+			}
+			else if (*blockLayout != ' ')
+			{
+				block->exists = true;
+				bool isCheckpoint = (*blockLayout == 'c');
+				block->color = (isCheckpoint) ? 0xAA5555 : 0xAAAAAA;
+				block->isCheckpoint = isCheckpoint;
+			}
+			else
+			{
+				ClearBlock(block);
+			}
+
+			block->halfSize = blockHalfSize;
+			block->position = blockPosition;
+
+			blockPosition.x += blockWidth;
+		}
+
+		if (!endOfContent)
+		{
+			blockLayout++;
+		}
+	}
+
+	return 0;
+}
