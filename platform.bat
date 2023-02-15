66122205d2e7759bbb6e26b090b75d1016739120
josborn5 <josborn5@gmail.com>
2023-02-14 22:13:56 -0600

    Simplifying platform test script.

--- platform.bat
+++ platform.bat
@@ -8,8 +8,6 @@ rmdir /S /Q %OUTPUT_DIR%
 mkdir %OUTPUT_DIR%
 pushd %OUTPUT_DIR%
 
-set FLAG=%1
-
 REM Build the libary .obj files
 REM using the '/c' flag to skip linking and create only the '.obj' file
 cl.exe %COMPILER_FLAGS% /c ..\%CODE_DIR%\platform\%TL_PLATFORM%.cpp
--- test.bat
+++ test.bat
@@ -1,30 +1,6 @@
 @echo off
 
-set CODE_DIR=src
-set OUTPUT_DIR=bin
+call ./platform.bat %1
 
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-
-rmdir /S /Q %OUTPUT_DIR%
-mkdir %OUTPUT_DIR%
-pushd %OUTPUT_DIR%
-
-REM https://docs.microsoft.com/en-us/cpp/build/reference/compiler-options-listed-alphabetically
-set COMMON_COMPILER_FLAGS=-MT -nologo -Gm- -GR- -EHa- -Oi -WX -W4 -wd4100 -wd4201 -FC -Z7 /EHsc -Fm
-
-set PLATFORM=toms-lane-platform
-
-REM Build the libary .obj files
-REM using the '/c' flag to skip linking and create only the '.obj' file
-cl.exe %COMMON_COMPILER_FLAGS% /c ..\%CODE_DIR%\platform\%PLATFORM%.cpp
-
-REM use the 'lib.exe' tool to create lib files from the obj files
-lib.exe -nologo %PLATFORM%.obj
-
-set COMMON_LINKER_FLAGS=-opt:ref user32.lib Gdi32.lib winmm.lib
-REM Build tests
-cl.exe %COMMON_COMPILER_FLAGS% ..\%CODE_DIR%\platform\toms-lane-platform.tests.cpp /link %COMMON_LINKER_FLAGS% %PLATFORM%.lib
-
-popd
-
-call "./bin/toms-lane-platform.tests.exe"
+call .\build-scripts\compile-project.bat toms-lane-platform.tests platform\toms-lane-platform.tests.cpp %1
+call .\build-scripts\run-project.bat toms-lane-platform.tests  %1
