55e6b65433844fed79ccc246d02f8eddee195fc5
josborn5 <josborn5@gmail.com>
2023-03-14 22:05:00 -0500

    sprite-editor reads the given filepath (command line arg)

--- src/sprite-editor/sprite-editor-win32.cpp
+++ src/sprite-editor/sprite-editor-win32.cpp
@@ -1,13 +1,25 @@
 #include <windows.h>
 #include "../win32/toms-lane-win32.hpp"
 
+char* filePath;
+
 int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR commandLine, int showCode)
 {
 	tl::WindowSettings settings;
 	settings.title = "Sprite Editor";
-	settings.width = 1280;
-	settings.height = 720;
+	settings.width = 800;
+	settings.height = 600;
 	settings.targetFPS = 60;
+	settings.permanentSpaceInMegabytes = 1;
+
+	if (!*commandLine)
+	{
+		return 1;
+	}
+
+	filePath = commandLine;
 
 	tl::Win32Main(instance, settings);
+
+	return 0;
 }
--- src/sprite-editor/sprite-editor.cpp
+++ src/sprite-editor/sprite-editor.cpp
@@ -1,8 +1,25 @@
 #include "../platform/toms-lane-platform.hpp"
+#include "../win32/toms-lane-win32.hpp"
 #include "./sprite-editor-win32.cpp"
 
 int tl::Initialize(const GameMemory& gameMemory, const RenderBuffer& renderBuffer)
 {
+	// Load file
+	tl::MemorySpace fileReadMemory;
+	fileReadMemory.sizeInBytes = gameMemory.PermanentStorageSpace;
+	fileReadMemory.content = gameMemory.PermanentStorage;
+
+	uint64_t fileSize = 0;
+	if (tl::GetFileSize(filePath, fileSize) != tl::Success)
+	{
+		return 1;
+	}
+
+	if (tl::ReadFile(filePath, fileReadMemory) != tl::Success)
+	{
+		return 1;
+	}
+	
 	return 0;
 }
 
