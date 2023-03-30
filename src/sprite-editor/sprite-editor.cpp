11c8c2e5f38bdec3e4f7d00a84c0bc950638c9de
josborn5 <josborn5@gmail.com>
2023-03-29 22:32:03 -0500

    Fixing the reading of the sprite from the file in sprite-editor

--- src/sprite-editor/sprite-editor.cpp
+++ src/sprite-editor/sprite-editor.cpp
@@ -2,7 +2,7 @@
 #include "../win32/toms-lane-win32.hpp"
 #include "./sprite-editor-win32.cpp"
 
-tl::SpriteC* sprite;
+tl::SpriteC sprite;
 tl::Rect<float> rootGrid;
 
 int tl::Initialize(const GameMemory& gameMemory, const RenderBuffer& renderBuffer)
@@ -11,6 +11,10 @@ int tl::Initialize(const GameMemory& gameMemory, const RenderBuffer& renderBuffe
 	tl::MemorySpace fileReadMemory;
 	fileReadMemory.sizeInBytes = gameMemory.PermanentStorageSpace;
 	fileReadMemory.content = gameMemory.PermanentStorage;
+
+	tl::MemorySpace tempMemory;
+	tempMemory.sizeInBytes = gameMemory.TransientStorageSpace;
+	tempMemory.content = gameMemory.TransientStorage;
 
 	uint64_t fileSize = 0;
 	if (tl::GetFileSize(filePath, fileSize) != tl::Success)
@@ -23,9 +27,14 @@ int tl::Initialize(const GameMemory& gameMemory, const RenderBuffer& renderBuffe
 		return 1;
 	}
 
-	sprite = (tl::SpriteC*)fileReadMemory.content;
+	char* spriteCharArray = (char*)fileReadMemory.content;
+	uint64_t spriteCharArrayLength = fileSize / sizeof(char);
+	char* addressAfterSpriteCharArray = &spriteCharArray[spriteCharArrayLength];
 
-	float aspectRatio = (float)sprite->height / (float)sprite->width;
+	sprite.content = (tl::Color*)addressAfterSpriteCharArray;
+	tl::LoadSpriteC(spriteCharArray, tempMemory, sprite);
+
+	float aspectRatio = (float)sprite.height / (float)sprite.width;
 	if (aspectRatio >= 1)
 	{
 		rootGrid.halfSize.y = 600.0f * 0.5f;
@@ -36,13 +45,30 @@ int tl::Initialize(const GameMemory& gameMemory, const RenderBuffer& renderBuffe
 		rootGrid.halfSize.x = 800;
 		rootGrid.halfSize.y = aspectRatio * rootGrid.halfSize.x * 0.5f;
 	}
-	rootGrid.position = tl::CopyVec2(rootGrid.halfSize);
 
+	rootGrid.position = tl::CopyVec2(rootGrid.halfSize);
 	return 0;
 }
 
 int tl::UpdateAndRender(const GameMemory &gameMemory, const Input &input, const RenderBuffer &renderBuffer, float dt)
 {
 	tl::DrawRect(renderBuffer, 0xFF0000, rootGrid);
+
+	tl::Rect<float> numberFoot;
+	numberFoot.halfSize = { 10.0f, 20.0f };
+	numberFoot.position = { 400.0f, 200.0f };
+	tl::DrawNumber(
+		renderBuffer,
+		(int)rootGrid.halfSize.x,
+		numberFoot,
+		0xFFFFFF
+	);
+	numberFoot.position.y -= 30.0f;
+	tl::DrawNumber(
+		renderBuffer,
+		(int)rootGrid.halfSize.y,
+		numberFoot,
+		0xFFFFFF
+	);
 	return 0;
 }
