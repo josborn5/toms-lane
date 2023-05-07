ae93044305962be3dea5febdd19699e2076fe55f
josborn5 <josborn5@gmail.com>
2023-05-06 22:57:05 -0500

    sprite-editor: multiple palettes.

--- src/sprite-editor/sprite-editor-palettes.cpp
+++ src/sprite-editor/sprite-editor-palettes.cpp
@@ -1,6 +1,8 @@
 #include "../platform/toms-lane-platform.hpp"
 #include "./editor.hpp"
 
+#define PALETTE_COUNT 2
+
 char* rgrPaletteContent = "\
 2\n\
 8\n\
@@ -21,11 +23,35 @@ char* rgrPaletteContent = "\
 255 191 64 255\n\
 204 20 36 255";
 
+char* pollenPaletteContent = "\
+2\n\
+4\n\
+115 70 76 255\n\
+171 86 117 255\n\
+238 106 124 255\n\
+255 167 165 255\n\
+255 224 126 255\n\
+255 231 214 255\n\
+114 220 187 255\n\
+52 172 186 255";
+
+char* paletteContents[PALETTE_COUNT] = {
+	rgrPaletteContent,
+	pollenPaletteContent
+};
+
+tl::SpriteC palettes[PALETTE_COUNT];
 tl::SpriteC rgrPalette;
 
-void InitializePalettes(const tl::MemorySpace& paletteMemory, tl::MemorySpace& tempMemory, EditorState& state)
+void InitializePalettes(tl::MemorySpace& paletteMemory, tl::MemorySpace& tempMemory, EditorState& state)
 {
-	rgrPalette.content = (tl::Color*)paletteMemory.content;
-	tl::LoadSpriteC(rgrPaletteContent, tempMemory, rgrPalette);
-	state.palette = &rgrPalette;
+	int totalPaletteMemorySize = 512;
+	int perPalleteMemorySize = totalPaletteMemorySize / PALETTE_COUNT;
+	for (int i = 0; i < PALETTE_COUNT; i += 1)
+	{
+		tl::MemorySpace paletteSpace = tl::CarveMemorySpace(perPalleteMemorySize, paletteMemory);
+		palettes[i].content = (tl::Color*)paletteSpace.content;
+		tl::LoadSpriteC(paletteContents[i], tempMemory, palettes[i]);
+	}
+	state.palette = &palettes[1];
 }
