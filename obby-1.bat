ddf931cd5b6ccc3bb2d43862abb55608653d005f
josborn5 <josborn5@gmail.com>
2023-05-22 22:37:12 -0500

    Updating player sprite and fixing build script to not ask about replacing file for obby-1

--- obby-1.bat
+++ obby-1.bat
@@ -1,9 +1,15 @@
 call .\build-scripts\compile-project.bat obby-1 obby\obby-1\obby-1.cpp %1
 
 pushd %OUTPUT_DIR%
+
+del .\brick.sprc
+del .\checkpoint.sprc
+del .\player.sprc
+
 xcopy ..\%CODE_DIR%\obby\brick.sprc .
 xcopy ..\%CODE_DIR%\obby\checkpoint.sprc .
 xcopy ..\%CODE_DIR%\obby\player.sprc .
+
 popd
 
 call .\build-scripts\run-project.bat obby-1 %1
