27255429d5b68ffc2e667ba091c7394ebf9edcd7
josborn5 <josborn5@gmail.com>
2023-11-26 13:52:06 -0600

    updating build scripts to no longer switch directories

--- platform.bat
+++ platform.bat
@@ -1,20 +1,18 @@
 @echo off
 
-call ./build-scripts/set-vcvars.bat
-call ./build-scripts/set-variables.bat %1
+set ROOT=%~dp0
+call "%ROOT%/build-scripts/set-vcvars.bat"
+call "%ROOT%/build-scripts/set-variables.bat" %1
 
 REM clear the output folder
 rmdir /S /Q %OUTPUT_DIR%
 mkdir %OUTPUT_DIR%
-pushd %OUTPUT_DIR%
 
 REM Build the libary .obj files
 REM using the '/c' flag to skip linking and create only the '.obj' file
-cl.exe %COMPILER_FLAGS% /c ..\%CODE_DIR%\platform\%TL_PLATFORM%.cpp
-cl.exe %COMPILER_FLAGS% /c ..\%CODE_DIR%\win32\%TL_WIN32%.cpp
+cl.exe %COMPILER_FLAGS% /Fo"%OUTPUT_DIR%" /c "%CODE_DIR%/platform/%TL_PLATFORM%.cpp"
+cl.exe %COMPILER_FLAGS% /Fo"%OUTPUT_DIR%" /c "%CODE_DIR%/win32/%TL_WIN32%.cpp"
 
 REM use the 'lib.exe' tool to create lib files from the obj files
-lib.exe -nologo %TL_PLATFORM%.obj
-lib.exe -nologo %TL_WIN32%.obj
-
-popd
+lib.exe -nologo "%OUTPUT_DIR%/%TL_PLATFORM%.obj"
+lib.exe -nologo "%OUTPUT_DIR%/%TL_WIN32%.obj"
