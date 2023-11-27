b138d6ddb4383d36cf3c78a6ed10620f03c5286b
josborn5 <josborn5@gmail.com>
2023-11-26 23:26:37 -0600

    win32: starting to move time to its own c-object

--- src/win32/sound/toms-lane-win32-directsound.cpp
+++ src/win32/sound/toms-lane-win32-directsound.cpp
@@ -1,4 +1,5 @@
 #include <dsound.h>
+#include <stdint.h>
 
 namespace tl
 {
--- src/win32/toms-lane-win32-time.cpp
+++ src/win32/toms-lane-win32-time.cpp
@@ -5,6 +5,34 @@ inline LARGE_INTEGER Win32_GetWallClock()
 	return Result;
 }
 
+struct Win32TimeTemp
+{
+	int64_t _performanceCountsPerSecond;
+};
+
+Win32TimeTemp win32_time_interface_initialize()
+{
+	LARGE_INTEGER perfCounterFrequencyResult;
+	QueryPerformanceFrequency(&perfCounterFrequencyResult);
+	Win32TimeTemp timeTemp;
+	timeTemp._performanceCountsPerSecond = perfCounterFrequencyResult.QuadPart;
+	return timeTemp;
+}
+
+double win32_time_interface_secondselapsed_get(const Win32TimeTemp& time, LARGE_INTEGER Start, LARGE_INTEGER End)
+{
+	uint64_t counterElapsed = End.QuadPart - Start.QuadPart;
+	double secondsElapsedForWork = ((double)counterElapsed / (double)time._performanceCountsPerSecond);
+	return secondsElapsedForWork;
+}
+
+int win32_time_interface_microsecondselapsed_get(const Win32TimeTemp& time, LARGE_INTEGER start)
+{
+	LARGE_INTEGER end = Win32_GetWallClock();
+	double secondsElapsed = win32_time_interface_secondselapsed_get(time, start, end);
+	return (int)(secondsElapsed * 1000000.0f);
+}
+
 class Win32Time
 {
 public:
