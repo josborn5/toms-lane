e8fb64d5fd3da07556d91d6ae4e48d180499032a
josborn5 <josborn5@gmail.com>
2023-11-28 22:37:35 -0600

    build: forgot sprite-editor

--- sprite-editor.bat
+++ sprite-editor.bat
@@ -1,4 +1,9 @@
-call ./build-scripts/set-variables.bat
+call .\build-scripts\set-variables.bat %1
 
-call .\build-scripts\compile-project.bat sprite-editor sprite-editor\sprite-editor.cpp %1
-call .\build-scripts\run-project.bat sprite-editor  %1
+set NAME=sprite-editor
+set APP_DIR=%~dp0\bin-sprite-editor
+
+call .\build-scripts\compile.bat "%~dp0\src\sprite-editor\sprite-editor.cpp", "%APP_DIR%" %1
+call .\build-scripts\link.bat "%APP_DIR%\sprite-editor.obj" "%~dp0\bin-tl-platform\toms-lane-platform.obj" "%~dp0\bin-tl-win32\toms-lane-win32.obj"
+call .\build-scripts\run.bat "%APP_DIR%\%NAME%.exe" %1
+
