1c14b42ee395555abd6cbaffe812d8ca612f4659
josborn5 <josborn5@gmail.com>
2024-01-15 00:10:23 -0600

    sound-test: stubbing out a new test app that uses console and no window

---
+++ src/sound-test/sound-test.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+#include "../win32/win32-console.hpp"
+
+int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR commandLine, int showCode)
+{
+	tl::console_interface_open();
+}
