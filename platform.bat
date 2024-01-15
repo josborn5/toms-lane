8dbba1906d7a67998484a2eef211aa9ad5b04c01
josborn5 <josborn5@gmail.com>
2024-01-14 23:50:36 -0600

    win32: console is it's own module and is no longer part of win32.lib

--- platform.bat
+++ platform.bat
@@ -4,9 +4,10 @@ call .\build-scripts\compile.bat "%~dp0\src\platform\toms-lane-platform.cpp" "%~
 
 call .\build-scripts\compile.bat "@%~dp0\src\win32\file-manifest.txt" "%~dp0\bin-tl-win32" %1
 
+call .\build-scripts\compile.bat "%~dp0\src\win32\console\toms-lane-win32-console.cpp" "%~dp0\bin-tl-win32-console" %1
+
 lib.exe /OUT:"%~dp0\bin-tl-win32\tl-win32.lib"^
  "%~dp0\bin-tl-win32\toms-lane-win32.obj"^
- "%~dp0\bin-tl-win32\toms-lane-win32-console.obj"^
  "%~dp0\bin-tl-win32\toms-lane-win32-file.obj"^
  "%~dp0\bin-tl-win32\toms-lane-win32-time.obj"^
  "%~dp0\bin-tl-win32\toms-lane-win32-directsound.obj"
