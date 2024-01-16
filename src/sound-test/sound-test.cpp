5131b3bb0d7bf94bccef3b19778534e874c38707
josborn5 <josborn5@gmail.com>
2024-01-15 18:39:33 -0600

    win32: fixing winmmsound!

--- src/sound-test/sound-test.cpp
+++ src/sound-test/sound-test.cpp
@@ -1,7 +1,34 @@
 #include <windows.h>
+#include <math.h>
 #include "../win32/win32-console.hpp"
+#include "../win32/win32-sound.hpp"
+
+int UpdateSound(const tl::SoundBuffer& soundBuffer)
+{
+	float tSineWave = soundBuffer.firstSampleTime;
+	int toneVolume = 1500;
+	int wavePeriod = soundBuffer.samplesPerSecond / 256;
+	int16_t* sampleOutput = soundBuffer.samples;
+
+	for (int i = 0; i < soundBuffer.sampleCount; i += 1)
+	{
+		float sineValue = sinf(tSineWave);
+		int16_t sampleValue = (int16_t)(sineValue * toneVolume);
+		*sampleOutput = sampleValue;
+		sampleOutput++;
+		tSineWave += 2.0f * 3.14159f * 1.0f / (float)wavePeriod;
+	}
+	return 0;
+}
 
 int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR commandLine, int showCode)
 {
 	tl::console_interface_open();
+
+	tl::win32_sound_interface_initialize(0, &UpdateSound);
+
+	while (true)
+	{
+
+	}
 }
