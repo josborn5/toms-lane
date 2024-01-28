dec2a587d59ac87d5105fccdc083f24c401e2d37
josborn5 <josborn5@gmail.com>
2024-01-27 20:43:36 -0600

    breakout: using an ADSR envelope to shape the sound a little

--- src/breakout/sound.cpp
+++ src/breakout/sound.cpp
@@ -2,6 +2,15 @@
 #include "../win32/win32-sound.hpp"
 #include <math.h>
 
+struct ASDREnvelope
+{
+	int attackDuration;
+	double attackAmplitude;
+	int decayDuration;
+	double sustainAmplitude;
+	int releaseDuration;
+};
+
 struct Tone
 {
 	int durationCount;
@@ -11,6 +20,7 @@ struct Tone
 };
 
 static Tone activeTone;
+static ASDREnvelope activeEnvelope;
 
 static const int samplesPerSecond = 44100;
 static const int samplesPerCallback = 512; // TODO: work out sound card latency and optimize
@@ -19,9 +29,50 @@ static double max16BitValue = 32767;
 
 static int samplesPerMillisecond = samplesPerSecond / 1000;
 
+double getEnvelopeAmplitude(const Tone& tone, const ASDREnvelope& envelope)
+{
+	int lastAttackSample = envelope.attackDuration;
+	int lastDecaySample = lastAttackSample + envelope.decayDuration;
+	int lastSustainSample = tone.durationCount - envelope.releaseDuration;
+	int lastSample = tone.durationCount;
+
+	if (tone.sampleCounter > lastSample)
+	{
+		return 0.0;
+	}
+
+	if (tone.sampleCounter > lastSustainSample)
+	{
+		double releaseGradient = envelope.sustainAmplitude / (double)envelope.releaseDuration;
+		double releaseTime = (double)(tone.sampleCounter - lastSustainSample);
+		return envelope.sustainAmplitude - (releaseGradient * releaseTime);
+	}
+
+	if (tone.sampleCounter > lastDecaySample)
+	{
+		return envelope.sustainAmplitude;
+	}
+
+	if (tone.sampleCounter > lastAttackSample)
+	{
+		double decayGradient = (envelope.attackAmplitude - envelope.sustainAmplitude) / (double)envelope.decayDuration;
+		double decayTime = (double)(tone.sampleCounter - lastDecaySample);
+		return envelope.attackAmplitude - (decayGradient * decayTime);
+	}
+
+	double attackGradient = envelope.attackAmplitude / (double)envelope.attackDuration;
+	return attackGradient * (double)tone.sampleCounter;
+}
+
 void playTone(int toneHz, int durationInMilliseconds)
 {
-	activeTone.durationCount = samplesPerMillisecond * durationInMilliseconds;
+	activeEnvelope.attackDuration = 200 * samplesPerMillisecond;
+	activeEnvelope.attackAmplitude = 1.0;
+	activeEnvelope.sustainAmplitude = 0.8;
+	activeEnvelope.releaseDuration = 200 * samplesPerMillisecond;
+	activeEnvelope.decayDuration = 200 * samplesPerMillisecond;
+
+	activeTone.durationCount = (samplesPerMillisecond * durationInMilliseconds) + activeEnvelope.attackDuration + + activeEnvelope.decayDuration + activeEnvelope.releaseDuration;
 	activeTone.sampleCounter = 0;
 	activeTone.toneHz = toneHz;
 	activeTone.volume = 0.15;
@@ -37,7 +88,8 @@ int UpdateSound(const tl::SoundBuffer& soundBuffer)
 		if (activeTone.sampleCounter < activeTone.durationCount)
 		{
 			double soundValue = activeTone.volume * sin(activeTone.sampleCounter * activeTone.toneHz * 2.0 * pi / (double)samplesPerSecond);
-			soundValueAs16Bit = max16BitValue * soundValue;
+			double envelopeFactor = getEnvelopeAmplitude(activeTone, activeEnvelope);
+			soundValueAs16Bit = max16BitValue * soundValue * envelopeFactor;
 		}
 
 		*sampleOutput = (int16_t)soundValueAs16Bit;
