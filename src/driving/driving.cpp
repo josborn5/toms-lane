95de8a52304c81db35599c155a7da2e76a9d739b
josborn5 <josborn5@gmail.com>
2024-01-28 19:03:14 -0600

    driving: using new window API

--- src/driving/driving.cpp
+++ src/driving/driving.cpp
@@ -1,5 +1,6 @@
+#include <windows.h>
+#include "../win32/toms-lane-win32.hpp"
 #include "../platform/toms-lane-platform.hpp"
-#include "main_win32.cpp"
 
 #include "driving.hpp"
 
@@ -43,4 +44,33 @@ int tl::Initialize(const GameMemory &gameMemory, const RenderBuffer &renderBuffe
 	gameState.tiles[2] = rightGrass;
 
 	return 0;
+}
+
+tl::GameMemory garbage;
+
+int updateWindowCallback(const tl::Input& input, int dtInMilliseconds, tl::RenderBuffer& renderBuffer)
+{
+	float dt = (float)dtInMilliseconds / 1000.0f;
+	return tl::UpdateAndRender(garbage, input, renderBuffer, dt);
+}
+
+int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR commandLine, int showCode)
+{
+	const int targetFPS = 60;
+
+	tl::WindowSettings settings;
+	settings.title = "Driving";
+	settings.width = 1280;
+	settings.height = 720;
+
+	int windowOpenResult = tl::OpenWindow(instance, settings);
+	if (windowOpenResult != 0)
+	{
+		return windowOpenResult;
+	}
+
+	tl::RenderBuffer garbagerender;
+	tl::Initialize(garbage, garbagerender);
+
+	return tl::RunWindowUpdateLoop(targetFPS, &updateWindowCallback);
 }
