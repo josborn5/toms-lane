02ad9d9695f132e9ee19e54bdba8c2df7aa9fc0b
josborn5 <josborn5@gmail.com>
2024-05-27 12:53:10 -0500

    platform: starting to add bitmap rendering

--- src/platform/bitmap.cpp
+++ src/platform/bitmap.cpp
@@ -11,4 +11,12 @@ int bitmap_interface_initialize(bitmap& bitmap, const MemorySpace& memory)
 	return 0;
 }
 
+int bitmap_interface_render(
+	const RenderBuffer& buffer,
+	const bitmap& bitmap,
+	Vec2<int> topLeftCornerPosition)
+{
+	return 0;
+}
+
 }
--- src/platform/bitmap.tests.cpp
+++ src/platform/bitmap.tests.cpp
@@ -1,15 +1,20 @@
 #include <assert.h>
 #include "./bitmap.hpp"
 
-void RunBitmapTests()
+static MemorySpace testMemory;
+static bitmap testBitmap;
+
+static void OneTimeSetup()
 {
-	MemorySpace memory;
-	bitmap testBitmap;
-	memory.sizeInBytes = 1024;
-	tl::memory_interface_initialize(memory);
-	int fileReadResult = tl::file_interface_read("../src/platform/test.bmp", memory);
+	testMemory.sizeInBytes = 1024;
+	tl::memory_interface_initialize(testMemory);
+	int fileReadResult = tl::file_interface_read("../src/platform/test.bmp", testMemory);
 	assert(fileReadResult == 0);
-	tl::bitmap_interface_initialize(testBitmap, memory);
+}
+
+static void RunBitmapInitializeTest()
+{
+	tl::bitmap_interface_initialize(testBitmap, testMemory);
 
 	assert(testBitmap.header->fileType == 0x4d42);
 	assert(testBitmap.header->fileSizeInBytes == 342);
@@ -31,6 +36,27 @@ void RunBitmapTests()
 	assert(testBitmap.header->verticalPixelsPerMeter == 0);
 	assert(testBitmap.header->numberOfColorsInPalette == 0);
 	assert(testBitmap.header->numberOfImportantColors == 0);
+}
 
+void RunBitmapRenderTest()
+{
+	RenderBuffer renderBuffer;
+	renderBuffer.width = 30;
+	renderBuffer.height = 20;
+	renderBuffer.bytesPerPixel = 4;
+	MemorySpace renderBufferPixels;
+	renderBufferPixels.sizeInBytes = sizeof(unsigned int) * renderBuffer.width * renderBuffer.height;
+	tl::memory_interface_initialize(renderBufferPixels);
+
+	tl::bitmap_interface_render(renderBuffer, testBitmap, { 0, 0 });
+}
+
+void RunBitmapTests()
+{
+	OneTimeSetup();
+
+	RunBitmapInitializeTest();
+
+	RunBitmapRenderTest();
 }
 
