51a7b8cd2b9f38d60cff0e1d97ae2c8970684121
josborn5 <josborn5@gmail.com>
2025-03-31 21:48:03 -0500

    platform: don't need DeleteFromArray<T>

--- src/platform/utilities.hpp
+++ src/platform/utilities.hpp
@@ -21,29 +21,6 @@ namespace tl
 	char* CopyToChar(char* from, char* to, const char toChar);
 	char* CopyToEndOfNumberChar(char* from, char* to);
 	char* GetNextNumberChar(char* from);
-
-	template<typename T>
-	int DeleteFromArray(T* content, unsigned int inclusiveStartIndex, unsigned int inclusiveEndIndex, unsigned int length)
-	{
-		if (inclusiveStartIndex >= length ||
-			inclusiveEndIndex >= length ||
-			inclusiveStartIndex > inclusiveEndIndex)
-		{
-			return -1;
-		}
-
-		unsigned int startCopyIndex = inclusiveEndIndex + 1;
-		unsigned int offset = startCopyIndex - inclusiveStartIndex;
-		for (unsigned int sourceIndex = startCopyIndex; sourceIndex < length; sourceIndex += 1)
-		{
-			unsigned int targetIndex = sourceIndex - offset;
-			T copyValue = content[sourceIndex];
-			content[targetIndex] = copyValue;
-		}
-
-		unsigned int subtractionCount = inclusiveEndIndex - inclusiveStartIndex + 1;
-		return subtractionCount;
-	}
 }
 
 #endif
