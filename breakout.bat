c8a7e77ce4a79560edd71e7d211ccfae3fb4ca18
josborn5 <josborn5@gmail.com>
2025-06-21 14:43:21 -0500

    don't needs to change working directory to call cmake

--- breakout.bat
+++ breakout.bat
@@ -1,9 +1,5 @@
-pushd cmake-build
+cmake -S . -B ./cmake-build
 
-cmake ../
-
-cmake --build .
-
-popd
+cmake --build ./cmake-build
 
 call "%~dp0\build-scripts\run.bat" "%~dp0/cmake-build/Debug/breakout-win32.exe" %1
