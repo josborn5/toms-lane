247fa857ff5989f62f28277797ce1ce84d40e78b
josborn5 <josborn5@gmail.com>
2025-12-18 22:50:20 -0600

    demo: tests grab a view matrix

--- src/demo/camera.tests.cpp
+++ src/demo/camera.tests.cpp
@@ -1,3 +1,21 @@
-void run_camera_tests() {
+#include <assert.h>
+#include "./camera.hpp"
 
+void run_camera_tests() {
+	tl::Vec3<float> p0 = { 0.0f, 0.0f, 0.0f };
+	tl::Vec3<float> p1 = { 0.0f, 1.0f, 0.0f };
+	tl::Vec3<float> p2 = { 1.0f, 1.0f, 0.0f };
+
+	camera_reset(
+		1.0f,
+		{ 0.5f, 0.5f, -5.0f },
+		45.0f,
+		1.0f,
+		10.0f,
+		{ 0.0f, 0.0f, 1.0f },
+		{ 0.0f, 1.0f, 0.0f }
+	);
+	
+	tl::Matrix4x4<float> view_matrix;
+	camera_fill_view_matrix(view_matrix);
 }
