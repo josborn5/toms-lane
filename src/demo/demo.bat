496ef0ec585413c56405063d5cd9c864b143cfdf
josborn5 <josborn5@gmail.com>
2025-12-27 11:41:17 -0600

    demo: is its own CMake project

--- src/demo/demo.bat
+++ src/demo/demo.bat
@@ -1,2 +1,24 @@
-call "%~dp0../../build-scripts/build-or-test.bat" demo-win32.exe demo-tests.exe %1
+SET CONFIG=Release
+SET TEST=false
+if "%1"=="-d" (
+	SET CONFIG=Debug
+)
+if "%1"=="-dt" (
+	SET CONFIG=Debug
+	SET TEST=true
+)
+if "%1"=="-t" (
+	SET TEST=true
+)
+
+cmake -S "%~dp0/." -B "%~dp0/cmake-build" || exit 1
+
+cmake --build "%~dp0/cmake-build" --config %CONFIG% || exit 1
+
+if %TEST%==true (
+	"%~dp0/cmake-build/%CONFIG%/demo-tests.exe"
+) else (
+	"%~dp0/cmake-build/%CONFIG%/demo-win32.exe"
+)
+
 
