4535bb2c9dbdcf7c2b036167cbe2637966539ff0
josborn5 <josborn5@gmail.com>
2026-01-01 15:24:42 -0600

    application: moving files

---
+++ application/application.bat
@@ -0,0 +1,11 @@
+SET CONFIG=Release
+if "%1"=="-d" (
+	SET CONFIG=Debug
+)
+
+cmake -S "%~dp0/src/win32/test" -B "%~dp0/src/win32/test/cmake-build" || exit 1
+
+cmake --build "%~dp0/src/win32/test/cmake-build" --config %CONFIG% || exit 1
+
+"%~dp0/src/win32/test/cmake-build/%CONFIG%/toms-lane-win32-application-tests.exe"
+
---
+++ application/src/tl-application.hpp
@@ -0,0 +1,17 @@
+#ifndef TOMS_LANE_APPLICATION_HPP
+#define TOMS_LANE_APPLICATION_HPP
+
+#include "./tl-console.hpp"
+#include "./tl-file.hpp"
+#include "./tl-memory.hpp"
+#include "./tl-sound.hpp"
+#include "./tl-window.hpp"
+#include "./tl-input.hpp"
+#include "./tl-text.hpp"
+
+namespace tl
+{
+	int main(char* commandLine);
+}
+
+#endif
