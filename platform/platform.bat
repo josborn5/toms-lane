d26cfe06533d920462fa330bf42ab96b74eb4646
josborn5 <josborn5@gmail.com>
2026-01-02 15:28:28 -0600

    fix running tests

--- platform/platform.bat
+++ platform/platform.bat
@@ -7,5 +7,5 @@ cmake -S "%~dp0/src/test" -B "%~dp0/src/test/cmake-build" || exit 1
 
 cmake --build "%~dp0/src/test/cmake-build" --config %CONFIG% || exit 1
 
-"%~dp0/src/test/cmake-build/%CONFIG%/toms-lane-platform-test.exe"
+"%~dp0/src/test/cmake-build/%CONFIG%/toms-lane-platform-test.exe" || exit 1
 
--- sprite-editor/sprite-editor.bat
+++ sprite-editor/sprite-editor.bat
@@ -10,7 +10,7 @@ cmake -S "%~dp0/." -B "%~dp0/cmake-build" || exit 1
 cmake --build "%~dp0/cmake-build" --config %CONFIG% || exit 1
 
 if %TEST%==true (
-	"%~dp0/cmake-build/test/%CONFIG%/sprite-editor-tests.exe" || exit 1
+	"%~dp0/cmake-build/src/test/%CONFIG%/sprite-editor-tests.exe" || exit 1
 ) else (
 	"%~dp0/cmake-build/%CONFIG%/sprite-editor-win32.exe"
 )
