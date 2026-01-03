4559f7d863e631923ffed1f4023192b2c6011cf4
josborn5 <josborn5@gmail.com>
2026-01-02 18:50:34 -0600

    go.bat

--- compile.bat
+++ compile.bat
@@ -1,10 +1,4 @@
 cmake -S "%~dp0/." -B "%~dp0/cmake-build" || exit 1
 
-SET CONFIG=release
-SET DEBUG=%1
-if "%DEBUG%"=="-d" (
-	SET CONFIG=debug
-)
-
-cmake --build "%~dp0/cmake-build" --config %CONFIG% || exit 1
+cmake --build "%~dp0/cmake-build" --config Release || exit 1
 
