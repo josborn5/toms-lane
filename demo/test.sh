3a49049844c744c40ce28fb855cb6fbf07d811e2
john <josborn5@gmail.com>
2026-02-03 22:03:13 -0600

    demo: fix linux build

--- demo/test.sh
+++ demo/test.sh
@@ -4,7 +4,7 @@ cmake -S . -B ./cmake-build || exit 1
 
 cmake --build ./cmake-build --config Release || exit 1
 
-./cmake-build/src/test/demo-tests || exit 1
+./cmake-build/test/demo-tests || exit 1
 
 cd ..
 
