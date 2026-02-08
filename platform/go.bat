cf7df4c2d591c2aa6a27ddecb9d10f8eb6347f78
john <josborn5@gmail.com>
2026-02-07 18:22:12 -0600

    platform: src --> lib

--- platform/go.bat
+++ platform/go.bat
@@ -1,11 +1,6 @@
-SET CONFIG=Release
-if "%1"=="-d" (
-	SET CONFIG=Debug
-)
+cmake -S "%~dp0/lib/test" -B "%~dp0/lib/test/cmake-build" || exit 1
 
-cmake -S "%~dp0/src/test" -B "%~dp0/src/test/cmake-build" || exit 1
+cmake --build "%~dp0/lib/test/cmake-build" --config Release || exit 1
 
-cmake --build "%~dp0/src/test/cmake-build" --config %CONFIG% || exit 1
-
-"%~dp0/src/test/cmake-build/%CONFIG%/toms-lane-platform-test.exe" || exit 1
+"%~dp0/lib/test/cmake-build/Release/toms-lane-platform-test.exe" || exit 1
 
--- platform/lib.sh
+++ platform/lib.sh
@@ -1,6 +1,6 @@
-cmake -S ./src/test -B ./src/test/cmake-build || exit 1
+cmake -S ./lib/test -B ./lib/test/cmake-build || exit 1
 
-cmake --build ./src/test/cmake-build --config Release || exit 1
+cmake --build ./lib/test/cmake-build --config Release || exit 1
 
-./src/test/cmake-build/toms-lane-platform-test || exit 1
+./lib/test/cmake-build/toms-lane-platform-test || exit 1
 
