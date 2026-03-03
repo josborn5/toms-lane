d7c4651c2c453c98a5c0e761c3ceed751acb1260
john <josborn5@gmail.com>
2026-03-02 22:27:20 -0600

    demo: debug for linux

--- demo/linux.sh
+++ demo/linux.sh
@@ -1,6 +1,7 @@
-cmake -S sdl2 -B sdl2/cmake-build || exit 1
+cmake -DCMAKE_BUILD_TYPE=Debug -S sdl2 -B sdl2/cmake-build || exit 1
 
-cmake --build sdl2/cmake-build --config Release || exit 1
+cmake --build sdl2/cmake-build --config Debug || exit 1
+
 
 sdl2/cmake-build/demo-linux
 
