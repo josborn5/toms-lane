6019e68badb227bffa1b3f7b316459a951dd2bca
josborn5 <josborn5@gmail.com>
2026-03-12 22:30:42 -0500

    breakout: fix windows build

--- breakout/go.bat
+++ breakout/go.bat
@@ -4,5 +4,5 @@ cmake -S "%~dp0/win32" -B "%~dp0/win32/cmake-build" || exit 1
 
 cmake --build "%~dp0/win32/cmake-build" --config %CONFIG% || exit 1
 
-"%~dp0/win32/cmake-build/src/%CONFIG%/breakout-win32.exe"
+"%~dp0/win32/cmake-build/%CONFIG%/breakout-win32.exe"
 
