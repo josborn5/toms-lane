c9b123f9741c0c96ad3ff46862911088544731f0
john <josborn5@gmail.com>
2026-03-14 16:37:58 -0500

    demo: default to build in release

--- demo/linux.sh
+++ demo/linux.sh
@@ -1,9 +1,8 @@
-config=Debug
+config=Release
 
 cmake -DCMAKE_BUILD_TYPE=$config -S sdl2 -B sdl2/cmake-build || exit 1
 
 cmake --build sdl2/cmake-build --config $config || exit 1
-
 
 sdl2/cmake-build/demo-linux
 
