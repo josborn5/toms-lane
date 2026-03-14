55199e3a059ecdc11184afeefc7974322e3cd346
john <josborn5@gmail.com>
2026-03-14 11:56:12 -0500

    demo: build in release mode. that fixes the segmentation faults seen in debug mode

--- demo/linux.sh
+++ demo/linux.sh
@@ -1,6 +1,6 @@
-cmake -DCMAKE_BUILD_TYPE=Debug -S sdl2 -B sdl2/cmake-build || exit 1
+cmake -DCMAKE_BUILD_TYPE=Release -S sdl2 -B sdl2/cmake-build || exit 1
 
-cmake --build sdl2/cmake-build --config Debug || exit 1
+cmake --build sdl2/cmake-build --config Release || exit 1
 
 
 sdl2/cmake-build/demo-linux
