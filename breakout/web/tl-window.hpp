e3a541591b670946168bbf7b1257cb0a1bae119c
john <josborn5@gmail.com>
2026-06-09 23:29:22 -0500

    breakout web: don't need

--- breakout/web/tl-window.hpp
+++ breakout/web/tl-window.hpp
@@ -4,14 +4,5 @@
 
 namespace tl
 {
-    struct WindowSettings {
-        int width;
-        int height;
-        const char* title;
-    };
-
     typedef int (*UpdateWindowCallback)(const Input& input, int dtInMilliseconds, VideoBuffer& videoBuffer);
-
-    int OpenWindow(const WindowSettings& settings);
-    int RunWindowUpdateLoop(int targetFPS, UpdateWindowCallback callback);
 }
